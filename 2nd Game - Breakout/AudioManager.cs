86b5d8e777e27069e24d6d11a31a60da371b521a
OndraCZ <ondracoding123@gmail.com>
2026-07-16 19:38:40 +0200

    AudioManager

--- 2nd Game - Breakout/AudioManager.cs
+++ 2nd Game - Breakout/AudioManager.cs
@@ -1,1 +1,56 @@
+using UnityEngine;
 
+public class AudioManager : MonoBehaviour
+{
+
+    AudioSource gameOverSound;
+    AudioSource gameCompletedSound;
+
+    [SerializeField] BallMovementScript ballMovementScript;
+
+    void Start()
+    {
+
+        gameOverSound = GetComponent<AudioSource>();
+        gameCompletedSound = GetComponent<AudioSource>();
+        
+    }
+
+    void Update()
+    {
+
+        PlayGameOverSound();
+        PlayGameCompletedSound();
+
+    }
+
+    void PlayGameOverSound()
+    {
+
+        if (!gameOverSound.isPlaying)
+        {
+
+            if (ballMovementScript.livesCounter == 0)
+            {
+
+                gameOverSound.Play();
+
+            }
+        }
+    }
+
+    void PlayGameCompletedSound()
+    {
+
+        if (!gameCompletedSound.isPlaying)
+        {
+
+            if (ballMovementScript.score == 240)
+            {
+
+                gameCompletedSound.Play();
+
+            }
+        }
+    }
+}
