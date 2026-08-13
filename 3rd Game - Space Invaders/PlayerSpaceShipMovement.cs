a172d7e6e63095f56d9818ea7c87ebc2ed8610eb
OndraCZ <ondracoding123@gmail.com>
2026-08-13 12:24:39 +0200

    Create PlayerSpaceShipMovement.cs

---
+++ 3rd Game - Space Invaders/PlayerSpaceShipMovement.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class PlayerSpaceShipMovement : MonoBehaviour
+{
+
+    float _moveSpeed = 5f;
+    public bool isMoving = false;
+    public float bulletMoveSpeed = 4f;
+
+    [SerializeField] GameManagerScript gameManagerScript;
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.A))
+        {
+
+            gameObject.transform.Translate(new Vector2(-_moveSpeed, 0f) * Time.deltaTime);
+
+            isMoving = true;
+
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+
+            gameObject.transform.Translate(new Vector2(_moveSpeed, 0f) * Time.deltaTime);
+
+            isMoving = true;
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+        {
+
+            isMoving = false;
+
+        }
+    }
+}
